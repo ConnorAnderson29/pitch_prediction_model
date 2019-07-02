fc1f62d39e97710c31e949d41b8edddc57e20fe4
ConnorAnderson29 <44457732+ConnorAnderson29@users.noreply.github.com>
2019-07-01 18:41:02 -0700

    Binarize target for improved accuracy

--- pitch_functions.py
+++ pitch_functions.py
@@ -1,6 +1,21 @@
 from sklearn.metrics import f1_score, accuracy_score, roc_auc_score
 from sklearn.preprocessing import LabelBinarizer
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def calc_roc_acc_score(y_test, y_pred, average='macro'):
     """Calculate binary/multiclass AUC
        Returns: AUC
@@ -11,8 +26,10 @@ def calc_roc_acc_score(y_test, y_pred, average='macro'):
     y_pred = lb.transform(y_pred)
     return roc_auc_score(y_test, y_pred, average=average)
    
+
+    
 def calc_acc_and_f1_score(true, preds, model_name='Model Name'):
-    """Calc classification metrics and print them out
+    """Calculate and print classification metrics
     """
     acc = accuracy_score(true, preds)
     f1 = f1_score(true, preds, average='weighted')
@@ -21,3 +38,22 @@ def calc_acc_and_f1_score(true, preds, model_name='Model Name'):
     print('Accuracy:{:.3f}'.format(acc))
     print('F1-Score: {:.3f}'.format(f1))
     print('AUC: {:.3f}'.format(multi_auc))
+
+
+def run_classifier_models(classifiers, X_train, X_test, y_train, y_test):
+    for classifier in classifiers:
+        ''' Intialize Pipeline, Fit Pipeline to Train Set''' 
+        clf1 = Pipeline(steps=[('preprocessor', preprocessor),
+                              ('classifier', classifier)])
+        clf1.fit(X_train, y_train)
+
+        ''' Calculate and display performance metrics''' 
+        print(classifier)
+        print('\n')
+        print('Training Metrics')
+        pitch_functions.calc_acc_and_f1_score(y_train, clf1.predict(X_train))
+        print('\n')
+        print('Testing Metrics')
+        pitch_functions.calc_acc_and_f1_score(y_test, clf1.predict(X_test))
+        print('\n')
+
