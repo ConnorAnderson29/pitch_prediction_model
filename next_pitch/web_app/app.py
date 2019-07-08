f5c5a83d4838f45aa297b59ae9ea3507ef8ff1d8
ConnorAnderson29 <44457732+ConnorAnderson29@users.noreply.github.com>
2019-07-08 15:25:42 -0700

    Add multiple user input boxes

--- next_pitch/web_app/app.py
+++ next_pitch/web_app/app.py
@@ -31,14 +31,29 @@ app = Flask(__name__, static_url_path="")
 @app.route("/")
 def index():
     """Return the main page."""
-    return render_template("index.html")
+    return render_template("new_index.html")
 
 
 @app.route("/output", methods=["GET", "POST"])
 def output():
     """Retun text from user input"""
+    #Python object to store user data
     data = request.get_json(force=True)
     # every time the user_input identifier
+    out = {"pitcher": data["pitcher"].value,
+                            'hitter': data["hitter"].value,
+                            'top/bottom': data["top/bottom"].value,
+                            'inning': data["inning"].value,
+                            'hitter_side': data["hitter_side"].value,
+                            'pitcher_side': data["pitcher_side"].value,
+                            'runners_on': data["runners_on"].value,
+                            'nasty': data["nasty"].value,
+                            'zone': data["zone"].value,
+                            'pitch_num': data["pitch_num"].value,
+                            'pitch_type': data["pitch_type"].value,
+                            'prior_pitch_type': data["prior_pitch_type"].value,
+                            'count': data["count"].value}
+    # output dictionary to web
     print(data)
     return jsonify(data["user_input"])
 
