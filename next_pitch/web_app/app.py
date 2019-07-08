f2a5bb63702d0e9366c5ced55385b886707fb0ec
ConnorAnderson29 <44457732+ConnorAnderson29@users.noreply.github.com>
2019-07-08 14:57:27 -0700

    Add initial flask app

--- next_pitch/web_app/app.py
+++ next_pitch/web_app/app.py
@@ -1,26 +1,44 @@
-import random
-import pandas as pd
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.pipeline import Pipeline
-import pickle
+# import random
+# import pandas as pd
+# from sklearn.feature_extraction.text import TfidfVectorizer
+# from sklearn.naive_bayes import MultinomialNB
+# from sklearn.pipeline import Pipeline
+# import pickle
+# from flask import Flask, request, render_template, jsonify
+
+
+# with open('final_test.pkl', 'rb') as f:
+#     model = pickle.load(f)
+# app = Flask(__name__, static_url_path="")
+
+# @app.route('/')
+# def index():
+#     """Return the main page."""
+#     return render_template('index.html')
+
+
+# @app.route('/predict', methods=['GET', 'POST'])
+# def predict():
+#     """Return a random prediction."""
+#     data = request.json
+#     prediction = model.predict_proba([data['user_input']])
+#     return jsonify({'probability': prediction[0][1]})
+
 from flask import Flask, request, render_template, jsonify
 
-
-with open('final_test.pkl', 'rb') as f:
-    model = pickle.load(f)
 app = Flask(__name__, static_url_path="")
 
-@app.route('/')
+@app.route("/")
 def index():
     """Return the main page."""
-    return render_template('index_gray.html')
+    return render_template("index.html")
 
 
-@app.route('/predict', methods=['GET', 'POST'])
-def predict():
-    """Return a random prediction."""
-    data = request.json
-    prediction = model.predict_proba([data['user_input']])
-    return jsonify({'probability': prediction[0][1]})
+@app.route("/output", methods=["GET", "POST"])
+def output():
+    """Retun text from user input"""
+    data = request.get_json(force=True)
+    # every time the user_input identifier
+    print(data)
+    return jsonify(data["user_input"])
 
