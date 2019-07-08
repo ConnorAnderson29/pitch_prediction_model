26d6e71a4d6b4ddfbb385265aeb95a7897ab774a
ConnorAnderson29 <44457732+ConnorAnderson29@users.noreply.github.com>
2019-07-08 16:19:41 -0700

    Add model to app.py, begin user input process

--- next_pitch/web_app/app.py
+++ next_pitch/web_app/app.py
@@ -1,32 +1,14 @@
-# import random
-# import pandas as pd
-# from sklearn.feature_extraction.text import TfidfVectorizer
-# from sklearn.naive_bayes import MultinomialNB
-# from sklearn.pipeline import Pipeline
-# import pickle
-# from flask import Flask, request, render_template, jsonify
-
-
-# with open('final_test.pkl', 'rb') as f:
-#     model = pickle.load(f)
-# app = Flask(__name__, static_url_path="")
-
-# @app.route('/')
-# def index():
-#     """Return the main page."""
-#     return render_template('index.html')
-
-
-# @app.route('/predict', methods=['GET', 'POST'])
-# def predict():
-#     """Return a random prediction."""
-#     data = request.json
-#     prediction = model.predict_proba([data['user_input']])
-#     return jsonify({'probability': prediction[0][1]})
-
+import pandas as pd
+from sklearn.pipeline import Pipeline
+import pickle
+import pitch_functions
+from flask import Flask, request, render_template, jsonify
 from flask import Flask, request, render_template, jsonify
 
 app = Flask(__name__, static_url_path="")
+
+with open('final_test.pkl', 'rb') as f:
+     model = pickle.load(f)
 
 @app.route("/")
 def index():
@@ -53,6 +35,9 @@ def output():
                             'pitch_type': data["pitch_type"].value,
                             'prior_pitch_type': data["prior_pitch_type"].value,
                             'count': data["count"].value}
+    
+    
+    
     # output dictionary to web
     print(data)
     return jsonify(data["user_input"])
