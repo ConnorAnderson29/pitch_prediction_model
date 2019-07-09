6cff43e7999aa36aec9a03603b7c7eddbe2de785
ConnorAnderson29 <44457732+ConnorAnderson29@users.noreply.github.com>
2019-07-08 18:55:14 -0700

    try to solve FLASK issues

--- next_pitch/web_app/app.py
+++ next_pitch/web_app/app.py
@@ -1,9 +1,7 @@
 import pandas as pd
 from sklearn.pipeline import Pipeline
 import pickle
-import pitch_functions2
-import library2
-import data_collection2
+
 
 from flask import Flask, request, render_template, jsonify
 
@@ -17,9 +15,29 @@ def index():
     """Return the main page."""
     return render_template("new_index.html")
 
+def merge_player_stats(dataframe):
+    '''Sources data from CSVs located in the main directory. 
+    Then it merges the data on the name of the player and returns their stats merged with pitches.'''
+    hitter_df = pd.read_csv('../../public_data/hitter_data.csv')
+    pitcher_df = pd.read_csv('../../public_data/pitcher_data.csv')
+    
+    hitter_df = hitter_df[['PLAYER', 'SLG', 'OPS', 'WAR']]
+    hitter_df = hitter_df.rename(columns={'PLAYER': 'hitter'})
+    
+    pitcher_df = pitcher_df[['PLAYER', 'WAR', 'WHIP', 'ERA', 'SO']]
+    pitcher_df = pitcher_df.rename(columns={'PLAYER': 'pitcher'})
+    
+    dataframe = dataframe.rename(columns={'matchup.batter.fullName': 'hitter',
+                                      'matchup.pitcher.fullName': 'pitcher'})
+    
+    merged = pd.merge(hitter_df, dataframe, on='hitter')
+    full_merge = pd.merge(pitcher_df, merged, on='pitcher')
+    
+    return full_merge
+
 def format_user_input(user_dict):
     live_df = pd.DataFrame.from_dict(user_dict)
-    created_test = data_collection.merge_player_stats(live_df)
+    created_test = merge_player_stats(live_df)
     created_test['pitchData.nastyFactor'] = 35.326
     created_test['pitchData.zone'] = 9.8751
     return created_test
@@ -29,14 +47,14 @@ def format_user_input(user_dict):
 def output():
     """Retun text from user input"""
     #Python object to store user data
-    data = request.get_json(force=True)
+    data = list(request.get_json(force=True))
     dictionary_of_data = format_user_input(data)
     preds = model.predict_probab(dictionary_of_data)
     off_speed_pred = preds[0][0]
     fastball_pred = preds[0][1]
     # every time the user_input identifier
 #     out = {"pitcher": data["pitcher"].value,
-#                             'hitter': data["hitter"].value,
+#                             'hitter': data['hitter'].value,
 #                             'top/bottom': data["top/bottom"].value,
 #                             'inning': data["inning"].value,
 #                             'hitter_side': data["hitter_side"].value,
